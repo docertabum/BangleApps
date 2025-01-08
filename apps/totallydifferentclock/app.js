15374d941fe1681629140c4250cea21c9ee92831
Marek Kudas <marek.kudas@t-systems.com>
2025-01-08 20:51:17 +0100

    fix

--- apps/totallydifferentclock/app.js
+++ apps/totallydifferentclock/app.js
@@ -21,9 +21,8 @@ function fetchWeather() {
         .then((gpsData) => {
             logDebug("Fetching with gps data: " + JSON.stringify(gpsData));
             let gpsString = JSON.stringify(gpsData);
-            let gpsJson = JSON.parse(gpsString);
-            console.log("GPS data: " + gpsJson.lon);
-            gps = JSON.parse(gpsData); // Store GPS data for later use
+            gps = JSON.parse(gpsString);
+            console.log("GPS data: " + gps.lon);
             logDebug("GPS.lon data: " + gps.lon);
             if (gps.lat !== 0 && gps.lon !== 0) {
                 let API_URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${gps.lat}&lon=${gps.lon}&units=metric&exclude=minutely,hourly,daily&appid=${config.appid}`;
